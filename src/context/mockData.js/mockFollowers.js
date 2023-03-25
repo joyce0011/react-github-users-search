e16ffa03fb9529746c6a0d05dd7d04aa9da72896
joyce0011 <545180731@qq.com>
2023-03-25 19:07:20 +0800

    initial commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,24 @@
+export default [
+  
+    {
+    login: "Rolemodel01291",
+    id: 84802341,
+    node_id: "MDQ6VXNlcjg0ODAyMzQx",
+    avatar_url: "https://avatars.githubusercontent.com/u/84802341?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/Rolemodel01291",
+    html_url: "https://github.com/Rolemodel01291",
+    followers_url: "https://api.github.com/users/Rolemodel01291/followers",
+    following_url: "https://api.github.com/users/Rolemodel01291/following{/other_user}",
+    gists_url: "https://api.github.com/users/Rolemodel01291/gists{/gist_id}",
+    starred_url: "https://api.github.com/users/Rolemodel01291/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/Rolemodel01291/subscriptions",
+    organizations_url: "https://api.github.com/users/Rolemodel01291/orgs",
+    repos_url: "https://api.github.com/users/Rolemodel01291/repos",
+    events_url: "https://api.github.com/users/Rolemodel01291/events{/privacy}",
+    received_events_url: "https://api.github.com/users/Rolemodel01291/received_events",
+    type: "User",
+    site_admin: false
+    }
+    
+];
